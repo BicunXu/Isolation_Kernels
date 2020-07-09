b357c7a4db107729aabf4c99503ac7b0c4d61ee2
xubc <xubc.gm@gmail.com>
2020-07-09 13:45:50 +0800

    first commit

--- IDK/convert_point_iNNE.m
+++ IDK/convert_point_iNNE.m
@@ -3,30 +3,8 @@ function all_im = convert_point( data,voros , t, psi)
 %% 
 num_bag = size(data,1);
 voro_num = size(voros,3);
-cache_size = floor(num_bag/100);
-cache = zeros(cache_size, psi*voro_num);
-all_im = [];
-for c=0:98
-    bias = c*cache_size;
-    for s=1:cache_size
-        i = bias+s;
-        im = zeros(t,psi);
-        index = voros(i,1,:);
-        index = reshape(index,1,t);
-        index_t = 1:t;
-        linear_index = sub2ind(size(im),index_t,index);
-        similarity = voros(i,3,:);
-        im(linear_index) = similarity;
-        im = reshape(im,1,psi*voro_num);
-        cache(s,:)=im;
-    end
-    cache_c = sparse(cache);
-    all_im = [all_im;cache_c];
-end
-cache = zeros(num_bag-cache_size*99, psi*voro_num);
-bias = 99*cache_size;
-for s=1:size(cache,1)
-    i = bias+s;
+all_im = zeros(num_bag, psi*voro_num);
+for i=1:num_bag
     im = zeros(t,psi);
     index = voros(i,1,:);
     index = reshape(index,1,t);
@@ -35,9 +13,7 @@ for s=1:size(cache,1)
     similarity = voros(i,3,:);
     im(linear_index) = similarity;
     im = reshape(im,1,psi*voro_num);
-    cache(s,:)=im;
+    all_im(i,:)=im;
 end
-cache = sparse(cache);
-all_im = [all_im;cache];
 end
 
